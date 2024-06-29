f42ee79f58c1bf2531cd22cbb763298485819df5
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-06-29 15:57:07 -0500

    feat(flake): configurable options, generate config

--- flake.nix
+++ flake.nix
@@ -19,51 +19,81 @@
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        packages.default =
-          pkgs.rustPlatform.buildRustPackage
-            rec {
-              pname = "persistwd";
-              version = "0.0.1";
+        packages = rec {
+          default = persistwd;
+          persistwd =
+            pkgs.rustPlatform.buildRustPackage
+              rec {
+                pname = "persistwd";
+                version = "0.0.1";
 
-              src = ./.;
-              cargoLock.lockFile = ./Cargo.lock;
+                src = ./.;
+                cargoLock.lockFile = ./Cargo.lock;
 
-              meta = {
-                description = "persistwd helps keep passwords persistent for non-mutable user setups";
-                homepage = "https://gitlab.com/xarvex/persistwd";
-                license = lib.licenses.mit;
-                maintainers = with lib.maintainers; [ xarvex ];
-                mainProgram = pname;
-                platforms = lib.platforms.linux;
+                meta = {
+                  description = "persistwd helps keep passwords persistent for non-mutable user setups";
+                  homepage = "https://gitlab.com/xarvex/persistwd";
+                  license = lib.licenses.mit;
+                  maintainers = with lib.maintainers; [ xarvex ];
+                  mainProgram = pname;
+                  platforms = lib.platforms.linux;
+                };
               };
+        };
+      };
+
+    flake.nixosModules.default = ({ config, lib, pkgs, ... }:
+      let
+        tomlFormat = pkgs.formats.toml { };
+      in
+      {
+        options.security.shadow.persistwd = {
+          enable = lib.mkEnableOption "persistwd";
+          package = lib.mkPackageOption self.packages.${pkgs.system} "persistwd";
+          settings = lib.mkOption {
+            type = tomlFormat.type;
+            default = {
+              users = builtins.mapAttrs (name: value: value.hashedPasswordFile) config.users.users;
             };
-      };
-
-    flake.nixosModules.default = ({ config, lib, pkgs, ... }: lib.mkIf (!config.users.mutableUsers) {
-      security.wrappers.passwd = {
-        setuid = true;
-        owner = "root";
-        group = "root";
-        source = "${config.security.loginDefs.package.out}/bin/passwd";
-      };
-
-      environment.etc."persistwd/config.toml".source = (pkgs.formats.toml { }).generate "config.toml" {
-        users = {
-          xarvex = "/persist/etc/shadow/xarvex";
+            example = lib.literalExpression ''
+              {
+                users = {
+                  root = "/persist/etc/shadow/root";
+                  xarvex = "/persist/etc/shadow/xarvex";
+                };
+              };
+            '';
+            description = ''
+              Configuration written to {file}`/etc/persistwd/config.toml`
+            '';
+          };
         };
-      };
 
-      systemd.services.persistwd = rec {
-        enable = true;
-        description = "persistwd helps keep passwords persistent for non-mutable user setups";
-        unitConfig = {
-          Description = [ description ];
-          After = [ "multi-user-pre.target" ];
-          PartOf = [ "multi-user.target" ];
-        };
-        serviceConfig.ExecStart = lib.getExe self.packages.${pkgs.system}.default;
-        wantedBy = [ "multi-user.target" ];
-      };
-    });
+        config =
+          let
+            cfg = config.security.shadow.persistwd;
+          in
+          lib.mkIf (config.security.shadow.enable && cfg.enable) {
+            security.wrappers.passwd = {
+              setuid = true;
+              owner = "root";
+              group = "root";
+              source = "${config.security.loginDefs.package.out}/bin/passwd";
+            };
+
+            systemd.services.persistwd = {
+              enable = true;
+              unitConfig = {
+                Description = [ "persistwd helps keep passwords persistent for non-mutable user setups" ];
+                After = [ "multi-user-pre.target" ];
+                PartOf = [ "multi-user.target" ];
+              };
+              serviceConfig.ExecStart = lib.getExe cfg.package;
+              wantedBy = [ "multi-user.target" ];
+            };
+
+            environment.etc."persistwd/config.toml".source = tomlFormat.generate "persistwd-settings" cfg.settings;
+          };
+      });
   };
 }
