f66afd58dae4f9d7b16cf28feac52dedc5a735cc
xarvex <gitlab-github.8qs1z@slmail.me>
2024-08-30 15:09:20 -0500

    feat(flake): remove impurity, update nix-direnv

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
   inputs = {
     devenv.url = "github:cachix/devenv";
 
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
+
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -64,6 +69,13 @@
             default = persistwd;
 
             persistwd = {
+              devenv.root =
+                let
+                  devenvRoot = builtins.readFile inputs.devenv-root.outPath;
+                in
+                # If not overriden (/dev/null), --impure is necessary.
+                pkgs.lib.mkIf (devenvRoot != "") devenvRoot;
+
               name = "persistwd";
 
               packages = with pkgs; [
